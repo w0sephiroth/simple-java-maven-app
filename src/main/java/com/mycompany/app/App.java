be0e5399a4e9773e3bbe24ba31068a1b1a933007
w0sephiroth <360924460@qq.com>
2019-08-05 10:53:49 +0800

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World22!";
+    private final String message = "Hello World223!";
 
     public App() {}
 
